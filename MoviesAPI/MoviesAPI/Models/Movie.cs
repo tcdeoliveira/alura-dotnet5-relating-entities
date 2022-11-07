3fa6744e84c9334eececd0874a82d55c91696c1b
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-11-07 20:18:19 +0000

    DEV | [02-05] Validating data

--- MoviesAPI/MoviesAPI/Models/Movie.cs
+++ MoviesAPI/MoviesAPI/Models/Movie.cs
@@ -4,9 +4,13 @@ namespace MoviesAPI.Models
 {
     public class Movie
     {
-        public string Titulo { get; set; }
-        public string Diretor { get; set; }
-        public string Genero { get; set; }
-        public int Duracao { get; set; }
+        [Required(ErrorMessage = "Title could not be null")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Diretor could not be null")]
+        public string Director { get; set; }
+        public string Category { get; set; }
+        [Range(1, 600, ErrorMessage = "Duration Range - min 1 and max 600")]
+        public int Duration { get; set; }
     }
 }
