69c11b398139b54ee1079a6b6bffa876db850f64
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-11-07 20:13:49 +0000

    DEV | Movies Controller, Model

---
+++ MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        [HttpPost]
+        public void Add([FromBody]Movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+    }
+}
