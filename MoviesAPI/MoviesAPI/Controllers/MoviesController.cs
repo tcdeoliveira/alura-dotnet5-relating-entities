435a291fca7156d028a12232f0460076a91bde46
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-11-08 10:58:05 +0000

    DEV | Get movies, Get movies by Id

--- MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MoviesAPI.Controllers
 {
@@ -8,11 +9,26 @@ namespace MoviesAPI.Controllers
     [Route("[controller]")]
     public class MoviesController : ControllerBase
     {
+        private static int id = 1;
+
         private static List<Movie> movies = new List<Movie>();
         [HttpPost]
         public void Add([FromBody]Movie movie)
         {
+            movie.Id = id++;
             movies.Add(movie);
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public Movie GetMovieById(int id)
+        {
+            return movies.FirstOrDefault(movie => movie.Id == id);
         }
     }
 }
--- MoviesAPI/MoviesAPI/Models/Movie.cs
+++ MoviesAPI/MoviesAPI/Models/Movie.cs
@@ -4,11 +4,14 @@ namespace MoviesAPI.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "Title could not be null")]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Diretor could not be null")]
         public string Director { get; set; }
+        [StringLength(30, ErrorMessage = "Max Length - 30 char")]
         public string Category { get; set; }
         [Range(1, 600, ErrorMessage = "Duration Range - min 1 and max 600")]
         public int Duration { get; set; }
