313a7a06b631736539d058d33645f56bdc6612dd
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-11-08 16:46:45 +0000

    DEV | [04-05] Migrations

---
+++ MoviesAPI/MoviesAPI/Data/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> opt) : base (opt)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Migrations/20221108162610_CreateMoviesTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace MoviesAPI.Migrations
+{
+    public partial class CreateMoviesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Director = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    Duration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
